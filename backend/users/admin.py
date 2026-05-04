7cdbd18628642efaceb23d707451b219fbbfc195
Hallory <khalpeep@gmail.com>
2026-05-04 11:40:05 +0200

    Register custom user in admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    search_fields = ('email', 'username','first_name','last_name')
+    ordering = ('email',)
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ("Custom fields", {"fields": ("role", "image")}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Custom fields", {"fields": ("email", "role", "image")}),
+    )
