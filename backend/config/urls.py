11b908a3f97925e2e884df206cfe97526c6b8509
Hallory <khalpeep@gmail.com>
2026-05-04 14:11:30 +0200

    Add properties API endpoints

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -23,6 +23,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("bookings.urls")),
+    path("api/", include("properties.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
--- backend/properties/views.py
+++ backend/properties/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .serializers import PropertySerializer
+from .models import Property
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
+    
+
+class PropertyViewSet(viewsets.ModelViewSet):
+    serializer_class = PropertySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        queryset =  Property.objects.select_related('owner').prefetch_related('amenities')
+        
+        if self.request.method in permissions.SAFE_METHODS:
+            return queryset.filter(status=Property.Status.PUBLISHED)
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+            
+            
