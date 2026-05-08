40e0e0e0f8f18536e552ebe6f4fe0218fc42ff5a
Hallory <khalpeep@gmail.com>
2026-05-08 09:38:44 +0200

    validate duplicate favorites

--- backend/favorites/serializers.py
+++ backend/favorites/serializers.py
@@ -4,18 +4,35 @@ from .models import Favorite
 
 
 class FavoriteSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.id')
+    user = serializers.ReadOnlyField(source="user.id")
+
+    def validate(self, attrs):
+        request = self.context.get("request")
+        rental_property = attrs.get("rental_property")
+
+        if (
+            request
+            and request.user.is_authenticated
+            and Favorite.objects.filter(
+                user=request.user, rental_property=rental_property
+            ).exists()
+        ):
+            raise serializers.ValidationError(
+                "You have already favorited this property."
+            )
+
+        return attrs
 
     class Meta:
         model = Favorite
         fields = [
-            'id',
-            'user',
-            'rental_property',
-            'created_at',
+            "id",
+            "user",
+            "rental_property",
+            "created_at",
         ]
         read_only_fields = [
-            'id',
-            'user',
-            'created_at',
+            "id",
+            "user",
+            "created_at",
         ]
